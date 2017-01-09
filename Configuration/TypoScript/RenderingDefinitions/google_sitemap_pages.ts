cc65710d17b1bc6056a6ee0f4f14deba3785aece
Thomas Löffler <loeffler@spooner-web.de>
2017-01-09 12:31:52 +0100

    [TASK] Adds separate page with type 711 for sitemap
    To prevent layout issues

--- Configuration/TypoScript/RenderingDefinitions/google_sitemap_pages.ts
+++ Configuration/TypoScript/RenderingDefinitions/google_sitemap_pages.ts
@@ -22,3 +22,7 @@ tt_content {
 config.disableAllHeaderCode = 1
 config.doctype = none
 config.additionalHeaders = Content-type:text/xml
+
+google_sitemap = PAGE
+google_sitemap.typeNum = 711
+google_sitemap.10 < styles.content.get
